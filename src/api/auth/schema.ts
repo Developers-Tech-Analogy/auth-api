d8d0dce2b5ddf321cf7c245a4e7584bbc32d222c
ABHILASH A <78740244+CodeWithABHILASH@users.noreply.github.com>
2022-02-18 00:58:55 +0530

    Update schema.ts

--- src/api/auth/schema.ts
+++ src/api/auth/schema.ts
@@ -1,11 +1,12 @@
 import * as yup from 'yup';
+import { emails } from './controller'
 const login = {
   email: yup.string().email().required(),
   password: yup.string().min(6).required(),
 };
 const signUp = {
   name: yup.string().required(),
-  email: yup.string().email().required(),
+  email: yup.string().email().required().notOneOf(emails,'Email address already taken!'),
   password: yup.string().min(6).required(),
 };
 
